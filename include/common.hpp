fb3359bf84b5c73bea45156348f478bbaa156d5f
yosswi414 <yosswi2@gmail.com>
2024-01-10 05:39:55 +0900

    impl-ed set operations

--- include/common.hpp
+++ include/common.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -15,7 +16,7 @@ class FileData : public TextData {
         if (from_stdin) *this = FileData(read_lines(std::cin), "stdin");
     }
     FileData(const std::string& fname) : TextData(read_lines(fname)), _filename(fname) {}
-    FileData(const TextData& td, const std::string& srcname): TextData(td), _filename(srcname) {}
+    FileData(const TextData& td, const std::string& srcname) : TextData(td), _filename(srcname) {}
     const std::string& name() const { return _filename; }
 
   private:
@@ -32,3 +33,44 @@ template <class T, class... Ts>
 void unused(T x, Ts... data) { unused(x), unused(data...); }
 
 const bool DEBUG_CERR = false;
+
+template <class T>
+std::set<T>& set_union(const std::set<T>& a, const std::set<T>& b, std::set<T>& res) {
+    res = a;
+    for (auto&& i : b) res.insert(i);
+    return res;
+}
+
+template <class T>
+std::set<T> set_union(const std::set<T>& a, const std::set<T>& b) {
+    std::set<T> res(a);
+    for (auto&& i : b) res.insert(i);
+    return res;
+}
+
+template <class T>
+std::set<T>& set_union_inplace(std::set<T>& a, const std::set<T>& b) {
+    for (auto&& i : b) a.insert(i);
+    return a;
+}
+
+template <class T>
+std::set<T>& set_minus(const std::set<T>& a, const std::set<T>& b, std::set<T>& res) {
+    res = a;
+    for (auto&& i : b) res.erase(i);
+    return res;
+}
+
+template <class T>
+std::set<T> set_minus(const std::set<T>& a, const std::set<T>& b) {
+    std::set<T> res(a);
+    for (auto&& i : b) res.erase(i);
+    return res;
+}
+
+template <class T>
+std::set<T>& set_minus_inplace(std::set<T>& a, const std::set<T>& b) {
+    for (auto&& i : b) a.erase(i);
+    return a;
+}
+
