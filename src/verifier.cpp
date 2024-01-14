a1c2ba0825f7eee56afa8ce2295853843a9b2acb
yosswi414 <yosswi2@gmail.com>
2024-01-15 03:04:32 +0900

    added file I/O

--- src/verifier.cpp
+++ src/verifier.cpp
@@ -1,8 +1,13 @@
 // objective: ./test_book3
 // generate a book from a given script
 
+#include <atomic>
+#include <chrono>
+#include <fstream>
 #include <iostream>
 #include <sstream>
+#include <thread>
+#include <cstdio>
 
 #include "common.hpp"
 #include "lambda.hpp"
@@ -13,14 +18,17 @@
     std::cerr << "with no FILE, read stdin. options:\n"
               << std::endl;
     std::cerr << "\t-f FILE     read FILE instead of stdin" << std::endl;
-    std::cerr << "\t-c          output book (judgements) in conventional notation" << std::endl;
-    std::cerr << "\t-n          output book in new notation" << std::endl;
-    std::cerr << "\t-r          output book in rich notation" << std::endl;
-    std::cerr << "\t-l LINES    read script until line LINES" << std::endl;
-    std::cerr << "\t-d def_file read def_file for definition reference" << std::endl;
-    std::cerr << "\t-v          verbose output for debugging purpose" << std::endl;
-    std::cerr << "\t-s          suppress output and just verify input (overrides -v)" << std::endl;
-    std::cerr << "\t-h          display this help and exit" << std::endl;
+    std::cerr << "\t-c                      output book (judgements) in conventional notation" << std::endl;
+    std::cerr << "\t-n                      output book in new notation" << std::endl;
+    std::cerr << "\t-r                      output book in rich notation" << std::endl;
+    std::cerr << "\t-l LINES                read script until line LINES" << std::endl;
+    std::cerr << "\t-d def_file             read def_file for definition reference" << std::endl;
+    std::cerr << "\t-o out_file             write output to out_file instead of stdout" << std::endl;
+    std::cerr << "\t--out-def out_def_file  write final environment to out_file" << std::endl;
+    std::cerr << "\t--skip-check            skip applicability check of inference rules" << std::endl;
+    std::cerr << "\t-v                      verbose output for debugging purpose" << std::endl;
+    std::cerr << "\t-s                      suppress output and just verify input (overrides -v)" << std::endl;
+    std::cerr << "\t-h                      display this help and exit" << std::endl;
     if (is_err) exit(EXIT_FAILURE);
     exit(EXIT_SUCCESS);
 }
@@ -33,10 +41,11 @@ enum Notation {
 
 int main(int argc, char* argv[]) {
     FileData data;
-    std::string fname(""), def_file("");
+    std::string fname(""), def_file(""), ofname(""), odefname("");
     int notation = Conventional;
     bool is_verbose = false;
     bool is_quiet = false;
+    bool skip_check = false;
     size_t limit = std::string::npos;
 
     for (int i = 1; i < argc; ++i) {
@@ -45,8 +54,17 @@ int main(int argc, char* argv[]) {
             if (arg == "-f") {
                 fname = std::string(argv[++i]);
                 continue;
-            }           else if (arg == "-d") {
+            } else if (arg == "-d") {
                 def_file = std::string(argv[++i]);
+                continue;
+            } else if (arg == "-o") {
+                ofname = std::string(argv[++i]);
+                continue;
+            } else if (arg == "--out-def") {
+                odefname = std::string(argv[++i]);
+                continue;
+            } else if (arg == "--skip-check") {
+                skip_check = true;
                 continue;
             } else if (arg == "-l") {
                 limit = std::stoi(argv[++i]);
@@ -73,12 +91,37 @@ int main(int argc, char* argv[]) {
     if (fname.size() == 0) data = FileData(true);
     else data = FileData(fname);
 
-    Book book;
+    Book book(skip_check);
     if (def_file.size() > 0) book.read_def_file(def_file);
 
     if (limit == std::string::npos) limit = data.size();
 
     std::stringstream ss;
+    auto alive1 = std::atomic_bool(true);
+    size_t last_size = 0, sec = 0, maxlen = 0;
+    auto th1 = std::thread([&alive1, &last_size, &sec, &maxlen, &book, &limit]() {
+        const std::chrono::milliseconds interval(1000);
+        while (alive1.load()) {
+            auto start = std::chrono::system_clock::now();
+            std::stringstream ss;
+            if (sec > 0) std::cerr << "\033[F" << '\r' << std::flush;
+            ss << "[" << sec++ << " secs]";
+            ss << "\tprogress: " << book.size() << " / " << limit
+               << " (+" << book.size() - last_size << " judgements/sec)";
+            std::string text = ss.str();
+            ss.clear();
+            ss.str("");
+            if (text.size() > maxlen) maxlen = text.size();
+            else text += std::string(maxlen - text.size(), ' ');
+            std::cerr << text << std::endl;
+            last_size = book.size();
+            auto end = std::chrono::system_clock::now();
+            auto waste = end - start;
+            if (waste < interval) std::this_thread::sleep_for(interval - waste);
+        }
+    });
+    th1.detach();
+
     for (size_t i = 0; i < limit; ++i) {
         ss << data[i];
         int lno;
@@ -212,21 +255,49 @@ int main(int argc, char* argv[]) {
                 "read error (token: " << op << ")",
                 __FILE__, __LINE__, __func__);
         }
-        if (!is_quiet && is_verbose) std::cout << book.string() << std::endl;
+        if (!is_quiet && is_verbose) {
+            std::cerr << book.string() << std::endl;
+        }
         ss.clear();
         ss.str("");
     }
 
-    if (!is_quiet) {
+    alive1.store(false);
+    std::cerr << "verification finished." << std::endl;
+
+    auto strbytes = [](size_t siz) {
+        std::stringstream ss;
+        double x = siz;
+        if (siz >= 1048576) {
+            x /= 1048576;
+            ss << (int)x;
+            ss << '.' << (int)((x - (int)x) * 10);
+            ss << " MB";
+            return ss.str();
+        }
+        if (siz >= 1024) {
+            x /= 1024;
+            ss << (int)x;
+            ss << '.' << (int)((x - (int)x) * 10);
+            ss << " KB";
+            return ss.str();
+        }
+        ss << siz << " Bytes";
+        return ss.str();
+    };
+
+    // std::ofstream ofs(ofname);
+    if (!is_quiet || ofname.size() > 0) {
+        std::string book_data;
         switch (notation) {
             case Conventional:
-                std::cout << book.repr();
+                book_data = book.repr();
                 break;
             case New:
-                std::cout << book.repr_new();
+                book_data = book.repr_new();
                 break;
             case Rich:
-                std::cout << book.string() << std::endl;
+                book_data = book.string();
                 break;
             default:
                 check_true_or_exit(
@@ -234,7 +305,54 @@ int main(int argc, char* argv[]) {
                     "invalid notation value = " << notation,
                     __FILE__, __LINE__, __func__);
         }
+        std::cerr << "outputting book data (" << strbytes(book_data.size()) << ")... ";
+        // if (ofs) {
+        //     ofs << book_data;
+        //     ofs.close();
+        // } 
+        if (ofname.size() > 0){
+            std::FILE* fp = std::fopen(ofname.c_str(), "wb");
+            std::fwrite(book_data.data(), sizeof(book_data[0]), book_data.size(), fp);
+            std::fclose(fp);
+        } else {
+            std::cout << book_data;
+        }
+        std::cerr << "done." << std::endl;
     }
+
+    if (odefname.size() > 0) {
+        std::string envrepr = book.back().env().repr();
+        std::cerr << "outputting def_file data (" << strbytes(envrepr.size()) << ")... ";
+        // std::ofstream odefs(odefname);
+        // odefs << envrepr;
+        // odefs.close();
+        std::FILE* fp = std::fopen(odefname.c_str(), "wb");
+        std::fwrite(envrepr.data(), sizeof(envrepr[0]), envrepr.size(), fp);
+        std::fclose(fp);
+    }
+
+    std::cerr << "done. " << std::endl;
+    sec = maxlen = 0;
+    auto alive2 = std::atomic_bool(true);
+    auto th2 = std::thread([&alive2, &sec, &maxlen]() {
+        const std::chrono::milliseconds interval(1000);
+        while (alive2.load()) {
+            auto start = std::chrono::system_clock::now();
+            std::stringstream ss;
+            if (sec > 0) std::cerr << "\033[F" << '\r' << std::flush;
+            ss << "[" << sec++ << " secs] waiting for file I/O...";
+            std::string text = ss.str();
+            ss.clear();
+            ss.str("");
+            if (text.size() > maxlen) maxlen = text.size();
+            else text += std::string(maxlen - text.size(), ' ');
+            std::cerr << text << std::endl;
+            auto end = std::chrono::system_clock::now();
+            auto waste = end - start;
+            if (waste < interval) std::this_thread::sleep_for(interval - waste);
+        }
+    });
+    th2.detach();
 
     return 0;
 }
